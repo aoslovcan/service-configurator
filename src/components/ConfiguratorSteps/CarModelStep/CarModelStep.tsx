f2e38d310f1d9032b20bab410a77828f63593a57
ERICSSON\eoslant <antonija.oslovcan@ericsson.com>
2023-01-28 15:49:12 +0100

    add first step

--- src/components/ConfiguratorSteps/CarModelStep/CarModelStep.tsx
+++ src/components/ConfiguratorSteps/CarModelStep/CarModelStep.tsx
@@ -1,38 +1,32 @@
-import React from "react";
-import ConfiguratorStep from "../../../common/ConfiguratorModal/ConfiguratorStep";
-import Form from "../../../common/Form/Form";
-import carModels from "../../../data/carBrand.json";
+import React from 'react'
+import Form from '../../../common/Form/Form'
+import carModels from '../../../data/carBrand.json'
+import FormInput from '../../../common/Form/FormInput'
 
-const CarModelStep = () => {
-
-    const handleOnClick = (e : React.MouseEvent<HTMLInputElement>) => {
-        console.log((e.target as HTMLInputElement).value)
-    }
-
-    return(
-        <ConfiguratorStep
-            stepTitle="Odaberite proizvođaća vašeg vozila"
-            className="step"
-            hidden={false}
-            stepNumber={1}
-        >
-        <div className="car-modal-step" data-testid="car-model-step">
-            <Form orientation="row" >
-                {carModels.cars.map(({ id, name }) => (
-                <div key={id} className="form-group">
-                    <input
-                        onClick={(e) => handleOnClick(e)}
-                        type="radio"
-                        id="carModel"
-                        value={name}
-                    />
-                    <label htmlFor="html">{name}</label>
-                </div>
-                ))}
-            </Form>
-        </div>
-        </ConfiguratorStep>
-    )
+type CarModeStepProps = {
+  handleButtonClick: (data: string) => void
 }
 
-export default CarModelStep;
+const CarModelStep = ({ handleButtonClick }: CarModeStepProps) => {
+  const handleOnClick = (e: string) => {
+    handleButtonClick(e)
+  }
+
+  return (
+    <div className="car-modal-step" data-testid="car-model-step">
+      <Form orientation="row">
+        {carModels.cars.map(({ id, name }) => (
+          <FormInput
+            key={id}
+            id={`car-model-input-${id}`}
+            inputType="radio"
+            handleButtonClick={(e) => handleOnClick(e)}
+            inputValue={name}
+          />
+        ))}
+      </Form>
+    </div>
+  )
+}
+
+export default CarModelStep
