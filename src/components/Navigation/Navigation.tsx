e8fc6140e7871789748654670af9c73ebfb903ed
ERICSSON\eoslant <antonija.oslovcan@ericsson.com>
2023-01-30 19:21:29 +0100

    add navigation component and style

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from '../../static/img/logo.png'
+
+const Navigation = () => {
+  return (
+    <div className="c-navigation row">
+      <div className="logo">
+        <img src={logo} />
+      </div>
+      <h4>KONFIGUATOR SERVISA</h4>
+    </div>
+  )
+}
+
+export default Navigation
