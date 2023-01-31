3afbedea075d7269f15ddcd02355f390a07459d1
ERICSSON\eoslant <antonija.oslovcan@ericsson.com>
2023-01-31 15:51:46 +0100

    refactor discount component

--- src/common/DiscountComponent/DiscountComponent.tsx
+++ src/common/DiscountComponent/DiscountComponent.tsx
@@ -1,11 +1,14 @@
-import React, { SetStateAction, useState, useEffect } from 'react'
-import { useFocus } from '../../helpers/customHooks'
+import React, { useState, useEffect } from 'react'
+import { useFocus, useForm } from '../../helpers/customHooks'
+import checkMark from '../../static/icons/checkmark.svg'
+
+type StateAction = (value: number) => void
 
 type DiscountComponent = {
   label: string
   price: number
-  setPrice: SetStateAction<any>
-  discount: SetStateAction<any>
+  setPrice: StateAction
+  discount: StateAction
 }
 
 const DiscountComponent = ({
@@ -15,11 +18,34 @@ const DiscountComponent = ({
   discount,
 }: DiscountComponent) => {
   const discountCode = 'Tokić123'
-  const errorMessage = 'Pogrešan kupon'
   const [discountCorrect, setDiscountCorrect] = useState(false)
-  const [messageShow, setMessageShow] = useState(false)
   const [isOpen, setIsOpen] = useState(false)
   const focusElement = useFocus()
+
+  const initialForm: Record<string, unknown> = {
+    discountValue: 0,
+  }
+
+  const validations = {
+    discountValue: {
+      required: {
+        value: true,
+        message: '',
+      },
+      custom: {
+        isValid: ({ discountValue }: { discountValue: string }) =>
+          discountValue === discountCode,
+        message: 'Pokrešan kod',
+      },
+    },
+  }
+
+  const { handleChange, formErrors } = useForm(
+    initialForm,
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    validations
+  )
 
   const discountPrice = (price: number) => {
     return (price / 100) * 30
@@ -35,8 +61,14 @@ const DiscountComponent = ({
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     const target = e.target as HTMLInputElement
     const inputValue = target.value
+
+    handleChange('discountValue')({
+      target: {
+        value: inputValue,
+      },
+    })
+
     setDiscountCorrect(inputValue.trim() === discountCode.trim())
-    setMessageShow(inputValue.trim() !== discountCode.trim())
   }
 
   const openDiscount = () => {
@@ -58,12 +90,24 @@ const DiscountComponent = ({
         <div className="form-element">
           <div className="row">
             <input
+              className="discountInput"
               ref={focusElement}
               type="text"
               onChange={(e) => handleInput(e)}
             />
-            <span>{!discountCorrect && messageShow ? errorMessage : null}</span>
-            {discountCorrect ? <span>&#10003;</span> : null}
+
+            <span className="validation-msg">
+              {
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                formErrors && formErrors['discountValue']
+              }
+            </span>
+            {discountCorrect ? (
+              <button className="c-button c-button-primary discountCheckButton">
+                <img src={checkMark} alt="" />
+              </button>
+            ) : null}
           </div>
         </div>
       ) : null}
